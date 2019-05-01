d1613b19573944b38be8dd63cc6a0527204ed0d6
bintangsenja <bintangsenjapratama@gmail.com>
2019-05-01 11:19:59 +0700

    fixing bug by adding deselect all before run loop

--- abcNapalm.py
+++ abcNapalm.py
@@ -16,11 +16,12 @@ except:
 # Need to make some interface and ability to enable all Collections viewport and render visibility in the scene
 # This script still need to select at least one object in the scene to run, didn't work when using bpy.ops.object.select_all(action='SELECT') before exporting Abc lines.
 
-# Export Abc for each Characters
+# Export Abc for Each Characters
 charList = ["hana", "oki", "omar"]
 
 for char in charList:
     if bpy.data.objects.get(char) is not None:
+        bpy.ops.object.select_all(action='DESELECT')
         charOb = bpy.data.objects[char]
         charOb.select_set(state=True)
         bpy.context.view_layer.objects.active = charOb
@@ -29,3 +30,6 @@ for char in charList:
         charOb.hide_viewport = False
         
         bpy.ops.wm.alembic_export(filepath="%s/%s_%s.abc" % (abcFolder, sceneName, char), check_existing=True, selected=True, renderable_only=False, visible_layers_only=False, flatten=True, uvs=True, packuv=False, normals=True,export_hair=False, export_particles=False, init_scene_frame_range=True)
+
+
+        #print("Yeay all of them exists in the scene")
