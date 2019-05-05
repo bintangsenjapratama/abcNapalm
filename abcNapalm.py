218518d439c6ab090f30dd5c3be50e1c649de0c7
bintangsenja <bintangsenjapratama@gmail.com>
2019-05-05 13:30:03 +0700

    renderable only True

--- abcNapalm.py
+++ abcNapalm.py
@@ -31,4 +31,4 @@ for char in charList:
         bpy.context.view_layer.objects.active = charOb
         
         
-        bpy.ops.wm.alembic_export(filepath="%s/%s_%s.abc" % (abcFolder, sceneName, char), check_existing=True, selected=True, renderable_only=False, visible_layers_only=False, flatten=True, uvs=True, packuv=False, normals=True,export_hair=False, export_particles=False, init_scene_frame_range=True)
+        bpy.ops.wm.alembic_export(filepath="%s/%s_%s.abc" % (abcFolder, sceneName, char), check_existing=True, selected=True, renderable_only=True, visible_layers_only=False, flatten=True, uvs=True, packuv=False, normals=True,export_hair=False, export_particles=False, init_scene_frame_range=True)
