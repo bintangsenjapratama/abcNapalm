3d9becdf911ba8fc3b89f785974108321d8c52ee
bintangsenja <bintangsenjapratama@gmail.com>
2019-05-16 06:05:20 +0700

    add enable all layers and asBackground False for alembic export

--- abcNapalm_2.7.py
+++ abcNapalm_2.7.py
@@ -15,6 +15,16 @@ except:
 
 # Need to make some interface and ability to enable all Collections viewport and render visibility in the scene
 
+for screen in bpy.data.screens:
+    for area in screen.areas:
+        if area.type == 'VIEW_3D':
+            for space in area.spaces:
+                if space.type == 'VIEW_3D':
+                    space.lock_camera_and_layers = True
+for scene in bpy.data.scenes:
+    layer = [l if l != False else True for l in scene.layers]
+    scene.layers = layer
+
 # Export Abc for Each Characters
 charList = ["hana", "oki", "omar", "ikan_koi_01", "ikan_koi_02", "ikan_koi_03"]
 bpy.ops.object.mode_set(mode='OBJECT')
@@ -29,6 +39,8 @@ for char in charList:
         charOb.hide = False
         charOb.select = True
         bpy.context.scene.objects.active = charOb
-        
-        
-        bpy.ops.wm.alembic_export(filepath="%s/%s_%s.abc" % (abcFolder, sceneName, char), check_existing=True, selected=True, renderable_only=True, visible_layers_only=False, flatten=True, uvs=True, packuv=False, normals=True,export_hair=False, export_particles=False, init_scene_frame_range=True)
+
+
+        bpy.ops.wm.alembic_export(filepath="%s/%s_%s.abc" % (abcFolder, sceneName, char), check_existing=True, selected=True, renderable_only=True, visible_layers_only=False, flatten=True, uvs=True, packuv=False, normals=True,export_hair=False, export_particles=False, init_scene_frame_range=True, as_background_job=False)
+
+bpy.ops.wm.quit_blender()
